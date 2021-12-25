e2b15f88d5ca108b5983bf1005d5ea66475842ed
NurYesiloz <76750159+NurYesiloz@users.noreply.github.com>
2021-12-26 00:38:23 +0300

    Add files via upload

---
+++ Feature_Engineering.py
@@ -0,0 +1,311 @@
+
+#################### Business Problem ####################
+# Required for a machine learning pipeline data preprocessing and
+# feature engineering script needs to be prepared.
+# When the dataset is passed through this script,
+# it is expected to be ready for the modeling.
+
+#################### Variables ############################
+# Survived ( 0 Died, 1 Survived)
+# Pclass – Ticket Class ( 1 = Grade 1, 2 = Grade 2, 3 = Grade 3)
+# Sex
+# Age
+# Sibsp – Number of siblings / spouses on the Titanic
+# Parch – Number of parents/children on Titanic
+# Embarked: – Passenger embarkation port (C = Cherbourg, Q = Queenstown, S = Southampton
+# Fare
+# Cabin – cabin number
+
+#################### Importing Libraries ###################
+
+import numpy as np
+import pandas as pd
+import seaborn as sns
+from matplotlib import pyplot as plt
+import missingno as msno
+from datetime import date
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import LocalOutlierFactor
+from sklearn.preprocessing import MinMaxScaler, LabelEncoder, StandardScaler, RobustScaler
+
+
+pd.set_option('display.max_columns', None)
+pd.set_option('display.float_format', lambda x: '%.3f' % x)
+pd.set_option('display.width', 500)
+
+#################### Loading the Dataset ####################
+
+def load():
+    data = pd.read_csv(r"C:\Users\WIN\OneDrive\Masaüstü\DSMLBC\datasets\titanic.csv")
+    return data
+df = load()
+
+#################### Data Overview ##########################
+def check_df(dataframe, head=5):
+    print("##################### Shape #####################")
+    print(dataframe.shape)
+    print("##################### Types #####################")
+    print(dataframe.dtypes)
+    print("##################### Head #####################")
+    print(dataframe.head(head))
+    print("##################### Tail #####################")
+    print(dataframe.tail(head))
+    print("##################### NA #####################")
+    print(dataframe.isnull().sum())
+    print("##################### Quantiles #####################")
+    print(dataframe.quantile([0, 0.05, 0.50, 0.95, 0.99, 1]).T)
+
+check_df(df, head=5)
+
+def grab_col_names(dataframe, cat_th=10, car_th=20):
+    # Returns the names of categorical, numeric and categorical but cardinal variables in the data set
+    # cat_cols, cat_but_car
+    cat_cols = [col for col in dataframe.columns if dataframe[col].dtypes == "O"]
+
+    num_but_cat = [col for col in dataframe.columns if dataframe[col].nunique() < cat_th and
+                   dataframe[col].dtypes != "O"]
+
+    cat_but_car = [col for col in dataframe.columns if dataframe[col].nunique() > car_th and
+                   dataframe[col].dtypes == "O"]
+
+    cat_cols = cat_cols + num_but_cat
+
+    cat_cols = [col for col in cat_cols if col not in cat_but_car]
+
+    # num_cols
+    num_cols = [col for col in dataframe.columns if dataframe[col].dtypes != "O"]
+
+    num_cols = [col for col in num_cols if col not in num_but_cat]
+
+    print(f"Observations: {dataframe.shape[0]}")
+    print(f"Variables: {dataframe.shape[1]}")
+    print(f'cat_cols: {len(cat_cols)}')
+    print(f'num_cols: {len(num_cols)}')
+    print(f'cat_but_car: {len(cat_but_car)}')
+    print(f'num_but_cat: {len(num_but_cat)}')
+
+    return cat_cols, num_cols, cat_but_car
+
+cat_cols, num_cols, cat_but_car = grab_col_names(df)
+
+cat_cols
+num_cols
+cat_but_car
+#################### Feature Engineering #####################
+df.columns = [col.upper() for col in df.columns]
+
+# Cabin bool
+df["NEW_CABIN_BOOL"] = df["CABIN"].notnull().astype('int')
+# Name count
+df["NEW_NAME_COUNT"] = df["NAME"].str.len()
+# name word count
+df["NEW_NAME_WORD_COUNT"] = df["NAME"].apply(lambda x: len(str(x).split(" ")))
+
+# name dr
+df["NEW_NAME_DR"] = df["NAME"].apply(lambda x: len([x for x in x.split() if x.startswith("Dr")]))
+
+# name title
+df['NEW_TITLE'] = df.NAME.str.extract(' ([A-Za-z]+)\.', expand=False)
+
+# family size
+df["NEW_FAMILY_SIZE"] = df["SIBSP"] + df["PARCH"] + 1
+
+# age_pclass
+df["NEW_AGE_PCLASS"] = df["AGE"] * df["PCLASS"]
+
+# is alone
+df.loc[((df['SIBSP'] + df['PARCH']) > 0), "NEW_IS_ALONE"] = "NO"
+df.loc[((df['SIBSP'] + df['PARCH']) == 0), "NEW_IS_ALONE"] = "YES"
+
+# age level
+df.loc[(df['AGE'] < 18), 'NEW_AGE_CAT'] = 'young'
+df.loc[(df['AGE'] >= 18) & (df['AGE'] < 56), 'NEW_AGE_CAT'] = 'mature'
+df.loc[(df['AGE'] >= 56), 'NEW_AGE_CAT'] = 'senior'
+
+# sex x age
+df.loc[(df['SEX'] == 'male') & (df['AGE'] <= 21), 'NEW_SEX_CAT'] = 'young_male'
+df.loc[(df['SEX'] == 'male') & ((df['AGE'] > 21) & (df['AGE']) <= 50), 'NEW_SEX_CAT'] = 'mature_male'
+df.loc[(df['SEX'] == 'male') & (df['AGE'] > 50), 'NEW_SEX_CAT'] = 'senior_male'
+df.loc[(df['SEX'] == 'female') & (df['AGE'] <= 21), 'NEW_SEX_CAT'] = 'young_female'
+df.loc[(df['SEX'] == 'female') & ((df['AGE'] > 21) & (df['AGE']) <= 50), 'NEW_SEX_CAT'] = 'mature_female'
+df.loc[(df['SEX'] == 'female') & (df['AGE'] > 50), 'NEW_SEX_CAT'] = 'senior_female'
+
+
+#################### Outliers #################################
+
+def outlier_thresholds(dataframe, col_name, q1=0.25, q3=0.75):
+    # Determination of the threshold value
+    quartile1 = dataframe[col_name].quantile(q1)
+    quartile3 = dataframe[col_name].quantile(q3)
+    interquantile_range = quartile3 - quartile1
+    up_limit = quartile3 + 1.5 * interquantile_range
+    low_limit = quartile1 - 1.5 * interquantile_range
+    return low_limit, up_limit
+
+def check_outlier(dataframe, col_name):
+    # Outlier detection
+    low_limit, up_limit = outlier_thresholds(dataframe, col_name)
+    if dataframe[(dataframe[col_name] > up_limit) | (dataframe[col_name] < low_limit)].any(axis=None):
+        return True
+    else:
+        return False
+
+def grab_outliers(dataframe, col_name, index=False):
+    # Access outliers.
+    low, up = outlier_thresholds(dataframe, col_name)
+    if dataframe[((dataframe[col_name] < low) | (dataframe[col_name] > up))].shape[0] > 10:
+        print(dataframe[((dataframe[col_name] < low) | (dataframe[col_name] > up))].head())
+    else:
+        print(dataframe[((dataframe[col_name] < low) | (dataframe[col_name] > up))])
+
+    if index:
+        outlier_index = dataframe[((dataframe[col_name] < low) | (dataframe[col_name] > up))].index
+        return outlier_index
+
+def remove_outlier(dataframe, col_name):
+    # Delete outliers
+    low_limit, up_limit = outlier_thresholds(dataframe, col_name)
+    df_without_outliers = dataframe[~((dataframe[col_name] < low_limit) | (dataframe[col_name] > up_limit))]
+    return df_without_outliers
+
+num_cols = [col.upper() for col in num_cols]
+num_cols = [col for col in num_cols if "PASSENGERID" not in col]
+
+for col in num_cols:
+    print("Column: ", col)
+    outlier_thresholds(df, col, q1=0.25, q3=0.75)
+    check_outlier(df, col)
+    grab_outliers(df, col, index=False)
+    remove_outlier(df, col)
+
+
+#################### Missing Values ###########################
+
+def missing_values_table(dataframe, na_name=False):
+    na_columns = [col for col in dataframe.columns if dataframe[col].isnull().sum() > 0]
+    n_miss = dataframe[na_columns].isnull().sum().sort_values(ascending=False)
+    ratio = (dataframe[na_columns].isnull().sum() / dataframe.shape[0] * 100).sort_values(ascending=False)
+    missing_df = pd.concat([n_miss, np.round(ratio, 2)], axis=1, keys=['n_miss', 'ratio'])
+    print(missing_df, end="\n")
+    if na_name:
+        return na_columns
+
+missing_values_table(df)
+
+df.drop("CABIN", inplace=True, axis=1)
+# Deleting the variables in the dataset that are not useful to us
+remove_cols = ["TICKET", "NAME"]
+df.drop(remove_cols, inplace=True, axis=1)
+df.head()
+
+missing_values_table(df)
+
+df["AGE"] = df["AGE"].fillna(df.groupby("NEW_TITLE")["AGE"].transform("median"))
+df["NEW_AGE_PCLASS"] = df["AGE"] * df["PCLASS"]
+df = df.apply(lambda x: x.fillna(x.mode()[0]) if (x.dtype == "O" and len(x.unique()) <= 10) else x, axis=0)
+df.isna().sum()
+
+#################### Encoding #################################
+
+binary_cols = [col for col in df.columns if df[col].dtype not in [int, float]
+                   and df[col].nunique() == 2]
+binary_cols
+
+def label_encoder(dataframe, binary_col):
+    labelencoder = LabelEncoder()
+    dataframe[binary_col] = labelencoder.fit_transform(dataframe[binary_col])
+    return dataframe
+
+for col in binary_cols:
+    df = label_encoder(df, col)
+df.head()
+cat_cols = [col.upper() for col in cat_cols]
+
+
+def rare_encoder(dataframe, rare_perc, cat_cols):
+
+    rare_columns = [col for col in cat_cols if (dataframe[col].value_counts() / len(dataframe) < 0.01).sum() > 1]
+
+    for col in rare_columns:
+        tmp = dataframe[col].value_counts() / len(dataframe)
+        rare_labels = tmp[tmp < rare_perc].index
+        dataframe[col] = np.where(dataframe[col].isin(rare_labels), 'Rare', dataframe[col])
+
+    return dataframe
+def rare_analyser(dataframe, target, cat_cols):
+    for col in cat_cols:
+        print(col, ":", len(dataframe[col].value_counts()))
+        print(pd.DataFrame({"COUNT": dataframe[col].value_counts(),
+                            "RATIO": dataframe[col].value_counts() / len(dataframe),
+                            "TARGET_MEAN": dataframe.groupby(col)[target].mean()}), end="\n\n\n")
+
+        def rare_encoder(dataframe, rare_perc, cat_cols):
+            rare_columns = [col for col in cat_cols if
+                            (dataframe[col].value_counts() / len(dataframe) < 0.01).sum() > 1]
+
+            for col in rare_columns:
+                tmp = dataframe[col].value_counts() / len(dataframe)
+                rare_labels = tmp[tmp < rare_perc].index
+                dataframe[col] = np.where(dataframe[col].isin(rare_labels), 'Rare', dataframe[col])
+
+            return dataframe
+cat_cols
+rare_analyser(df, "SURVIVED", cat_cols)
+df = rare_encoder(df, 0.01, cat_cols)
+df.head()
+
+def one_hot_encoder(dataframe, categorical_cols, drop_first=False):
+    dataframe = pd.get_dummies(dataframe, columns=categorical_cols, drop_first=drop_first)
+    return dataframe
+ohe_cols = [col for col in df.columns if 17 >= df[col].nunique() > 2]
+ohe_cols = [col.upper() for col in ohe_cols]
+df = one_hot_encoder(df, ohe_cols)
+df.head()
+
+
+
+#################### Standart Scaler #############################
+
+
+scaler = StandardScaler()
+df[num_cols] = scaler.fit_transform(df[num_cols])
+df[num_cols].head()
+df.head()
+df.tail()
+
+
+#################### Model #######################################
+
+# Choosing the dependent variable
+y = df["SURVIVED"]
+X = df.drop(["PASSENGERID", "SURVIVED"], axis=1)
+
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30, random_state=17)
+
+from sklearn.ensemble import RandomForestClassifier
+
+rf_model = RandomForestClassifier(random_state=46).fit(X_train, y_train)
+y_pred = rf_model.predict(X_test)
+accuracy_score(y_pred, y_test)
+
+
+#################### Feature Importance ############################
+
+
+def plot_importance(model, features, num=len(X), save=False):
+    feature_imp = pd.DataFrame({'Value': model.feature_importances_, 'Feature': features.columns})
+    plt.figure(figsize=(10, 10))
+    sns.set(font_scale=1)
+    sns.barplot(x="Value", y="Feature", data=feature_imp.sort_values(by="Value",
+                                                                     ascending=False)[0:num])
+    plt.title('Features')
+    plt.tight_layout()
+    plt.show()
+    if save:
+        plt.savefig('feature_importances.png')
+
+
+plot_importance(rf_model, X_train, num=30)
+
